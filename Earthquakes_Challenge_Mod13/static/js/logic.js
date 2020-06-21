d9dbce4b6d54e995ebd676ec50d3a2623fe546ff
Ammad Rashid <ammad@Ammads-MacBook-Pro-2.local>
2020-06-21 14:37:23 -0700

    Updated challenge files

--- Earthquakes_Challenge_Mod13/static/js/logic.js
+++ Earthquakes_Challenge_Mod13/static/js/logic.js
@@ -6,19 +6,19 @@ console.log("working");
 let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: API_Key
 });
 
 let satelliteStreets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: API_Key
 });
 
 let outdoors = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 		maxZoom: 18,
-		accessToken: API_KEY
+		accessToken: API_Key
 	});
 
 let baseMaps = {
